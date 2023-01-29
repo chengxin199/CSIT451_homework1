11dbe9250c43f16cb92cfce70e6999dd43f1a2a9
chengxin xu <chengxin199@gmail.com>
2023-01-29 10:26:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication_first/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication_first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btn_click(View v){
+        TextView tv_msg = findViewById(R.id.textView);
+
+        counter+=1;
+        tv_msg.setText("Hello World" + counter);
+    }
+}
